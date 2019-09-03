726c975acc2ad989931842ccb70dc1a0c0dcd558
TheseusNUL <45854859+TheseusNUL@users.noreply.github.com>
2019-09-03 13:35:24 +0100

    File Location Moved

---
+++ Battleship/Game.cpp
@@ -0,0 +1,255 @@
+#include "Game.h"
+#include <cstdlib>
+
+Game::Game(Player& player1, Player& player2)
+	: m_player1(player1)
+	, m_player2(player2)
+{
+	SetUpBoards(player1);
+	SetUpBoards(player2);
+
+	PlayGame(player1, player2);
+}
+
+bool Game::PlayAgain()
+{
+	char input;
+
+	const char validInput[2] = { 'y', 'n' };
+
+	input = GetCharInput("Would you like to play again? (y/n): ", INPUT_ERROR_STRING, validInput, 2, CC_LOWER_CASE);
+
+	return input == 'y';
+
+}
+
+void Game::PlayGame(Player& player1, Player& player2)
+{
+	Player* pCurrentPlayer = &player1;
+	Player* pOtherPlayer = &player2;
+
+	Vector2 guess;
+
+	do 
+	{
+		DrawBoards(*pCurrentPlayer);
+		bool isGuessValid;
+
+		do
+		{
+			std::cout << pCurrentPlayer->GetPlayerName() << " please make a guess" << std::endl;
+
+			guess = GetBoardPosition();
+			
+			isGuessValid = pCurrentPlayer->isGuessValid(guess);
+
+			if (!isGuessValid)
+			{
+				std::cout << "This was not a valid guess! Please try again." << std::endl;
+			}
+		} while (!isGuessValid);
+
+		Ship::ShipType type = pCurrentPlayer->UpdateBoards(*pOtherPlayer, guess);
+		DrawBoards(*pCurrentPlayer);
+
+		/* -------------------------------------------------------------------------------
+			index for type is type-1 due to enum of ships types being one out of sync with 
+			ships initialization with player ships member.
+		--------------------------------------------------------------------------------*/
+
+		if (type != Ship::ST_NONE && pOtherPlayer->ShipSunk(pOtherPlayer->GetShip(type - 1)))
+		{
+			std::cout << "You sunk " << pOtherPlayer->GetPlayerName() << "'s " << pOtherPlayer->GetShip(type-1).GetShipTypeName(type) << "!" << std::endl; 
+		}
+
+		system("pause");
+
+		SwitchPlayers(&pCurrentPlayer, &pOtherPlayer);
+
+	} while (!GameOver(player1, player2));
+
+	DisplayWinner(player1, player2);
+}
+
+void Game::SetUpBoards(Player& player)
+{
+	player.ClearBoards();
+
+	for (int i = 0; i < NUM_SHIPS; i++)
+	{
+		DrawBoards(player);
+
+		Ship& currentShip = player.GetShip(i);
+
+		Vector2 shipPos;
+		Ship::ShipOrientation shipOrentation;
+
+		bool isPlacementValid = false;
+
+		do
+		{
+			std::cout << player.GetPlayerName() << " set the position and orientation for your " << currentShip.GetShipTypeName(currentShip.GetShipType()) << std::endl;
+
+			shipPos = GetBoardPosition();
+			shipOrentation = currentShip.GetShipOrientation();
+
+			isPlacementValid = player.IsPlacementValid(currentShip, shipPos, shipOrentation);
+
+			if (!isPlacementValid)
+			{
+				std::cout << "Not a valid placement. Please try again." << std::endl;
+				system("pause");
+			}
+
+		} while (!isPlacementValid);
+
+		player.PlaceShip(currentShip, shipPos, shipOrentation);
+	}
+
+	DrawBoards(player);
+	system("pause");
+
+}
+
+
+bool Game::GameOver(Player& player1, Player& player2) const
+{
+	if (player1.AreAllShipsSunk() || player2.AreAllShipsSunk())
+		return true;
+	else
+		return false;
+}
+
+void Game::DrawBoards(Player& player)
+{
+	system("cls");
+
+	DrawColumnsRow();
+
+	DrawColumnsRow();
+
+	std::cout << std::endl;
+
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		DrawSeparatorLine();
+
+		std::cout << " ";
+
+		DrawSeparatorLine();
+
+		std::cout << std::endl;
+
+		DrawShipBoardRow(player, i);
+
+		std::cout << " ";
+
+		DrawGuessBoardRow(player, i);
+
+		std::cout << std::endl;
+	}
+
+	DrawSeparatorLine();
+
+	std::cout << " ";
+
+	DrawSeparatorLine();
+
+	std::cout << std::endl;
+}
+
+void Game::DrawSeparatorLine()
+{
+	std::cout << " ";
+
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		std::cout << "+---";
+	}
+
+	std::cout << "+";
+}
+
+void Game::DrawColumnsRow()
+{
+	std::cout << "  ";
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		int columnName = i + 1;
+
+		std::cout << " " << columnName << "  ";
+	}
+}
+
+void Game::DrawShipBoardRow(Player& player, int row)
+{
+	char rowName = row + 'A';
+	std::cout << rowName << "|";
+
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		std::cout << " " << player.GetShipRepresentation(row, i) << " |";
+	}
+}
+
+void Game::DrawGuessBoardRow(Player& player, int row)
+{
+	char rowName = row + 'A';
+	std::cout << rowName << "|";
+
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		std::cout << " " << player.GetGuessRepresentation(row, i) << " |";
+	}
+}
+
+Vector2 Game::GetBoardPosition()
+{
+	char rowInput;
+	int colInput;
+
+	const char validRowInputs[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+	const int validColumnInputs[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	rowInput = GetCharInput("Please input a row (A - J): ", INPUT_ERROR_STRING, validRowInputs, BOARD_SIZE, CC_UPPER_CASE);
+	colInput = GetIntInput("Please input a column (1 - 10): ", INPUT_ERROR_STRING, validColumnInputs, BOARD_SIZE);
+
+	return MapBoardPosition(rowInput, colInput);
+}
+
+Vector2 Game::MapBoardPosition(char rowInput, int colInput)
+{
+	int row = rowInput - 'A';
+	int col = colInput - 1;
+
+	Vector2 boardPos;
+
+	boardPos.m_row = row;
+	boardPos.m_col = col;
+
+	return boardPos;
+}
+
+void Game::SwitchPlayers(Player ** currentPlayer, Player ** otherPlayer)
+{
+	/*----------------------------------------------------------------
+						 ------->	player 1 ---
+						|						|
+		current player - (switch ptr)			 -------> Player
+												|
+									player 2 ---
+	----------------------------------------------------------------*/
+
+	Player * temp = *currentPlayer;
+	*currentPlayer = *otherPlayer;
+	*otherPlayer = temp;
+}
+
+void Game::DisplayWinner(Player& player1, Player& player2) const
+{
+	if (player1.AreAllShipsSunk())
+		std::cout << player2.GetPlayerName() << " is the winner. Congratulations!" << std::endl;
+	else
+		std::cout << player1.GetPlayerName() << " is the winner. Congratulations!" << std::endl;
+}
+
---
+++ Battleship/Game.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Player.h"
+#include "Console.h"
+#include "Utils.h"
+
+class Game
+{
+public:
+	Game(Player& player1, Player& player2);
+	bool PlayAgain();
+	void PlayGame(Player& player1, Player& player2);
+	void SetUpBoards(Player& player);
+	bool GameOver(Player& player1, Player& player2) const;
+
+	void DrawBoards(Player& player);
+	void DrawSeparatorLine();
+	void DrawColumnsRow();
+	void DrawShipBoardRow(Player& player, int row);
+	void DrawGuessBoardRow(Player& player, int row);
+
+	Vector2 GetBoardPosition();
+	Vector2 MapBoardPosition(char rowInput, int colInput);
+
+	void SwitchPlayers(Player ** currentPlayer, Player ** otherPlayer);
+	void DisplayWinner(Player& player1, Player& player2) const;
+
+private:
+
+	Player m_player1;
+	Player m_player2;
+};
